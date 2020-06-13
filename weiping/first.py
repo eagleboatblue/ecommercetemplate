3f074bbc5b8810b69749b21531941ef9877d756e
Weiping <weip2008@gmail.com>
2020-06-13 12:08:32 -0500

    add first.py

---
+++ weiping/first.py
@@ -0,0 +1,22 @@
+import yfinance as yf
+import matplotlib.pyplot as plt
+import seaborn
+
+msft = yf.Ticker("MSFT")
+
+# get stock info
+print(msft.info)
+
+# get historical market data
+hist = msft.history(period="5d")
+
+print(type(hist))
+
+# msft.actions
+
+# Plot everything by leveraging the very powerful matplotlib package
+hist['Close'].plot(figsize=(16, 9))
+
+# Download stock data then export as CSV
+data_df = yf.download("AAPL", start="2020-02-01", end="2020-03-20")
+data_df.to_csv('aapl.csv')
